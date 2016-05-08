17274b02a2eab2a7008dc20ffc92b91f9af81e7c
Maura Hart <mhart@essxgroup.com>
2016-05-08 13:43:12 -0400

    Sunday

--- Project2/js/app.js
+++ Project2/js/app.js
@@ -1,71 +1,67 @@
-
 //find out what news source was clicked
-$('li.container').click(function(e){
-    var clickedURL = $('a', this).attr('href');
-    console.log('clicked on ' + clickedURL);
-    return false; // you can use e.preventDefault(); instead of this.Same effect
-});
+var url1 = 'https://www.reddit.com/top.json';
+var url2 = 'http://api.npr.org/query?id=1001&apiKey=MDI0MjE0OTE3MDE0NjI2NTk2MTk4NzZiMQ000';
+var url3 = 'http://digg.com/api/news/popular.json;';
+var url = '';
 
-// $(document).ready(function() {
-var httpRequest = new XMLHttpRequest();
-httpRequest.onreadystatechange = responseMethod;
-function responseMethod() {
-    // Check if our state is "DONE"
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        // If our request was successful we get a return code/status of 200
-        if (httpRequest.status === 200) {
-            // This is where we update our UI accordingly. Our data is available to us through the
-            // responseText parameter
-            document.getElementById('Status').innerHTML = httpRequest.status;
-            document.getElementById('StatusText').innerHTML = httpRequest.statusText;
-            console.log(httpRequest.responseText);
-        } else {
-            // This is the scenario that there was an error with our request
-            console.log('There was a problem with the request.');
-        }
-    }
-}
+$(document).ready(function (event) {
+    $('.container li').click(function (e) {
+        /* Want to set the url equal to what is clicked
+        $('#News Source').change(function(e){
+         var clickedURL = $('a', this).attr('href');
+         console.log('clicked on ' + url);
+         }); */
+    });
 
+    url = url1;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = responseMethod;
 
-'use strict';
-//Get Financial Data Button Clicked
-//$('#getDataButton').click (function(event){
-//    event.preventDefault();
+    'use strict';
 
     $.ajax({
-        url: 'http://digg.com/api/news/popular.json',
-        data:{format: "json"},
-        success: function(response){
-            console.log(response);
+        url: url,
+        data: {format: "json"},
+        success: function (response) {
+            console.log('page loaded');
         },
-        error: function(response){
+        error: function (response) {
             console.log('error');
         }
     });
-//});
+    httpRequest.open('GET', url);
+    httpRequest.send();
 
+    function responseMethod(response) {
+        // Check if our state is "DONE"
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            // If our request was successful we get a return code/status of 200
+            if (httpRequest.status === 200) {
+                //var allData = response.data?response.data | [];
+               // var allData = (response.data==undefined ? response.data:[]);
+                    var responseObject;
+                    responseObject = JSON.parse(httpRequest.responseText);
+                    console.log('response object is ' + responseObject);
+                    var newContent = '';
+                    for (var i = 0; i < responseObject.length; i++) {
+                        newContent += '<div class="event">';
+                        newContent += '<img src="' + responseObject.events[i].map + '"';
+                       // newContent += 'alt="' + responseObject.events[i].location + '"/>';
+                       // newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
+                       // newContent += responseObject.events[i].dateTime + '</p>';
+                        newContent += '</div>';
 
-// Open method accepts 3 parameter:
-// 1. Request type: these are all the HTTP verbs we covered above
-// 2. The URL
-// 3. Optional boolean third parameter, that dictates wether this is an asynchronous call (default is true)
-httpRequest.open('GET', 'http://data.consumerfinance.gov/api/views.json');
-
-// The send method takes an optional parameter. If our API request allows additional parameters or JSON objects
-// to be passed through (primarily through POST requests), we pass them in the send method.
-httpRequest.send();
-
-/*$.ajax({
-    url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(FEED_URL),
-    dataType : 'json',
-    success  : function (data) {
-        if (data.responseData.feed && data.responseData.feed.entries) {
-            $.each(data.responseData.feed.entries, function (i, e) {
-                console.log("------------------------");
-                console.log("title      : " + e.title);
-                console.log("author     : " + e.author);
-                console.log("description: " + e.description);
-            });
+                    }
+                console.log('newContent is '+ newContent);
+             //   document.getElementById('name').innerHTML = newContent;
+                console.log(httpRequest.responseText);
+            } else {
+                // This is the scenario that there was an error with our request
+                console.log('There was a problem with the request.');
+            }
         }
     }
-}); */
+
+
+});
+
